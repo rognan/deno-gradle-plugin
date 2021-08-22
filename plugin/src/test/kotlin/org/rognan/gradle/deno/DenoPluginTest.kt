a77de95c7fc20b31d3e41a0a3bc3a614ce7a67a8
Thor Andreas Rognan <thor.rognan@gmail.com>
2021-08-22 16:04:27 +0200

    Add task 'denoInstall' to task group 'Build Setup' by default

--- plugin/src/test/kotlin/org/rognan/gradle/deno/DenoPluginTest.kt
+++ plugin/src/test/kotlin/org/rognan/gradle/deno/DenoPluginTest.kt
@@ -1,15 +1,41 @@
 package org.rognan.gradle.deno
 
+import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 import kotlin.test.assertNotNull
 
 class DenoPluginTest {
-  @Test
-  fun `task 'denoInstall' is registered`() {
-    val project = ProjectBuilder.builder().build()
-    project.plugins.apply("org.rognan.gradle.deno-plugin")
+  private lateinit var project: Project
 
+  @BeforeTest
+  fun setUp() {
+    project = ProjectBuilder.builder().build()
+    project.plugins.apply("org.rognan.gradle.deno-plugin")
+  }
+
+  @Test
+  fun `plugin registers task with name 'denoInstall'`() {
     assertNotNull(project.tasks.findByName("denoInstall"))
+  }
+
+  @Test
+  fun `task 'denoInstall' is located in the 'Build Setup' task group`() {
+    val group = project.tasks.findByName("denoInstall")
+      ?.group
+
+    assertEquals("Build Setup", group)
+  }
+
+  @Test
+  fun `task 'denoInstall' has a non-empty default description`() {
+    val description = project.tasks.findByName("denoInstall")
+      ?.description?.replace("\\s".toRegex(), "")
+
+    assertNotNull(description)
+    assertNotEquals("", description)
   }
 }
