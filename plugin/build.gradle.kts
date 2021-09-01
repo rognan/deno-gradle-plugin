dd5d140553c3c3075d2af07a47d11a35a1515dfa
Thor Andreas Rognan <thor.rognan@gmail.com>
2021-09-01 21:53:15 +0200

    Define project group

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -9,6 +9,8 @@ repositories {
   mavenCentral()
 }
 
+group = "org.rognan.gradle.deno-plugin"
+
 dependencies {
   implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
