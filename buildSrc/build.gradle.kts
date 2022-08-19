c09c712659ad69036170b60c3136ce607ecde687
Thor Andreas Rognan <thor@rognan.org>
2022-08-19 21:10:09 +0200

    Bump gradle-idea-ext plugin from 1.1.3 to 1.1.6

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -67,6 +67,6 @@ tasks.check {
 
 dependencies {
   // The 1.0 version of idea-ext requires IntelliJ IDEA ≥ 2020.3
-  implementation("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:1.1.3")
+  implementation("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:1.1.6")
   implementation("com.diffplug.spotless-changelog:spotless-changelog-plugin-gradle:2.4.0")
 }
