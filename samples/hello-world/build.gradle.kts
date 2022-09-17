c65f5bac5fe5a01c174c9c9e2804b01e07705fd8
Thor Rognan <thor.rognan@gmail.com>
2022-09-17 13:59:40 +0200

    Add sample hello-world

---
+++ samples/hello-world/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  id("base")
+  id("org.rognan.gradle.deno-plugin") version "0.1.0"
+}
+
+deno {
+  version.set("1.14.3")
+}
+
+tasks.register<org.rognan.gradle.deno.task.DenoExecTask>("helloWorld") {
+  args.set(listOf("eval", "console.log('Hello, World!');"))
+}
+
+tasks.named("build") {
+  dependsOn("helloWorld")
+}
