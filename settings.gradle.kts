6afbef7f6cc7fa59ca0f49e2e4ab756c6d9107ff
Thor Rognan <thor.rognan@gmail.com>
2022-09-17 13:59:41 +0200

    Activate Gradle build scans by default on CI
    This commit activates build scans on all CI-builds by default which
    are then subsequently published to the public Gradle build scan portal.
    
    For convenience; An html-file, automatically redirecting the user to
    the actual build scan, is created and uploaded as a build report for
    each workflow run. This form of redirect will work in any (semi-)modern
    browser.
    
    To ensure local copies of previous build-scan.html files are removed on
    `./gradlew clean` we apply the `base` plugin to the root project build
    file.
    
    In addition to information already gathered by default by gradle during
    the scan, (named) tags are utilized to provide:
    
    - A link back to the relevant branch or tag in the GitHub repository,
      for convenient navigation.
    - Commit SHA, so that we can check out the appropriate revision locally
      when debugging/comparing locally.
    - Which workflow and job that triggered the build in question, again
      for debugging purposes.
    
    In accordance with GDPR, and basic courtesy really, personal
    identifiable information, such as username, hostname and ip-addresses,
    are removed when the environment variable 'CI' is not set. This will
    allow people to create build scans without publishing their computer
    names, IP's, etc.
    
    I don't know what the value of the system property `user.name` would be
    on the GitHub Actions runner, but to achieve a constant username across
    CI-builds it's hardcoded to `github`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,65 @@ dependencyResolutionManagement {
   }
 }
 
+plugins {
+  id("com.gradle.enterprise") version "3.7.2"
+}
+
+val isCiServer = !env("CI").isNullOrEmpty()
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+
+    tag(if(isCiServer) "CI" else "Local")
+
+    if (isCiServer) {
+      termsOfServiceAgree = "yes"
+      publishAlways()
+
+      link("VCS", linkToGitHubTagOrBranch())
+
+      tag(env("GITHUB_REF_TYPE")) // branch or tag
+      value("Commit", env("GITHUB_SHA"))
+      value("Workflow", env("GITHUB_WORKFLOW"))
+      value("Job", env("GITHUB_JOB"))
+    }
+
+    buildScanPublished {
+      val shortcut = file("${rootDir}/build/reports/gradle-enterprise/build-scan.html")
+      shortcut.parentFile.mkdirs()
+      shortcut.writeText("""
+        <!DOCTYPE HTML>
+        <html>
+         <head>
+          <title>Build Scan</title>
+          <meta http-equiv="refresh" content="0; url='$buildScanUri'">
+         </head>
+         <body>Redirecting you to $buildScanUri.</body>
+        </html>
+        """.trimIndent())
+    }
+
+    obfuscation {
+      if (isCiServer) {
+        username { "github" }
+      } else {
+        username { "[hidden]" }
+        hostname { "[hidden]" }
+        ipAddresses { listOf("[hidden]") }
+      }
+    }
+  }
+}
+
 include(
   "plugin",
   "samples"
 )
 
 rootProject.name = "deno-gradle-plugin"
+
+fun env(variable: String) = System.getenv(variable)
+fun linkToGitHubTagOrBranch() = env("GITHUB_SERVER_URL") + // i.e. `https://github.com`
+  "/${env("GITHUB_REPOSITORY")}" + // i.e. `rognan/deno-gradle-plugin`
+  "/tree/${env("GITHUB_REF_NAME")}" // i.e. `tree/main`
