d00262b08b27cb803f081402cfa7393c15fa1947
Thor Rognan <thor.rognan@gmail.com>
2022-09-17 13:59:40 +0200

    Add the ability to execute deno through Gradle

--- plugin/src/main/kotlin/org/rognan/gradle/deno/task/InstallTask.kt
+++ plugin/src/main/kotlin/org/rognan/gradle/deno/task/InstallTask.kt
@@ -17,21 +17,51 @@
 package org.rognan.gradle.deno.task
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.WorkResult
 
+/**
+ * Unpacks given {@code archive} into {@code destinationDir}.
+ */
+// Making this task cacheable probably serves no purpose, the dependency itself is already cached so
+// there's no network overhead to speak of, and unzipping the file is about as expensive as
+// restoring it from a cache.
 open class InstallTask : DefaultTask() {
+
+  /**
+   * The zip archive to unpack into {@code destinationDir}.
+   */
+  @get:PathSensitive(PathSensitivity.ABSOLUTE)
+  @get:InputFile
+  val archive: RegularFileProperty = project.objects.fileProperty()
+
+  /**
+   * The directory to unpack {@code archive} into.
+   */
+  @get:OutputDirectory
+  val destinationDir: DirectoryProperty = project.objects.directoryProperty()
+
   init {
     group = DEFAULT_GROUP
     description = "Installs a local version of deno."
   }
 
   @TaskAction
-  fun install() {
-    println("Hello from plugin 'org.rognan.gradle.deno-plugin'")
+  fun install(): WorkResult {
+    return project.copy {
+      it.from(project.zipTree(archive.asFile))
+      it.into(destinationDir)
+    }
   }
 
   companion object {
-    const val NAME = "denoInstall"
     const val DEFAULT_GROUP = "Build Setup"
+    const val NAME = "denoInstall"
   }
 }
---
+++ plugin/src/main/kotlin/org/rognan/gradle/deno/util/DependencyHelper.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rognan.gradle.deno.util
+
+import java.util.Properties
+
+/**
+ * Provides utility functions that helps identify which platform specific deno dependency to fetch
+ * from the build assets available with each deno-release.
+ */
+class DependencyHelper(private val properties: Properties = System.getProperties()) {
+  fun organization(): String {
+    return "denoland"
+  }
+
+  fun module(): String {
+    return "deno"
+  }
+
+  fun classifier(): String {
+    return "${arch()}-${os()}"
+  }
+
+  fun extension(): String {
+    return "zip"
+  }
+
+  private fun arch(): String {
+    val arch: String = properties.getProperty("os.arch") ?: System.getProperty("os.arch")
+
+    return when {
+      arch.contains("amd64", true) -> "x86_64"
+      arch.contains("x86_64", true) -> "x86_64"
+      arch.contains("aarch64", true) -> "aarch64"
+      else -> error("Unsupported OS Architecture: $arch")
+    }
+  }
+
+  private fun os(): String {
+    val name: String = properties.getProperty("os.name") ?: System.getProperty("os.name")
+
+    return when {
+      name.contains("mac", true) -> "apple-darwin"
+      name.contains("win", true) -> "pc-windows-msvc"
+      name.contains("nix", true) -> "unknown-linux-gnu"
+      name.contains("nux", true) -> "unknown-linux-gnu"
+      name.contains("aix", true) -> "unknown-linux-gnu"
+      name.contains("bsd", true) -> "unknown-linux-gnu"
+      else -> error("Unsupported OS: $name")
+    }
+  }
+}
